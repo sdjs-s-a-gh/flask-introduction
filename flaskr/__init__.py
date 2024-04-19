9afff6bde6362d72f4b48ada1277a2a96b334062
sdjs-s-a-gh <164402991+sdjs-s-a-gh@users.noreply.github.com>
2024-04-19 19:36:45 +0100

    hopefully you actually commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,7 +10,7 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(os.path.join(file_path, "testing whether this actually works.accdb") + ";"),
     )
-
+    print("hello")
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
